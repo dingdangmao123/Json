632bcc8ce8bffd18156c6e6416f4e89c7a3427c6
dingdangmao123 <3228936343@qq.com>
2017-04-07 15:53:21 +0800

    fix array bug

--- Json.java
+++ Json.java
@@ -1,112 +1,134 @@
 public class Json{
-
-public static String parseString(String str,String k){
-
-	int start=0;
-	int end=0;
-	int len=str.length();
- 	int i=str.indexOf(k);
- 	i=i+k.length()+1;
- 	while(i<len){
- 		if(str.charAt(i)=='"'){
- 			     	start=i+1;
- 			     	break;
+	public static String parseString(String str,String k){
+		int start=0;
+		int end=0;
+		int len=str.length();
+		int i=str.indexOf(k);
+		i=i+k.length()+1;
+		while(i<len){
+			if(str.charAt(i)=='"')
+			{
+				start=i+1;
+				break;
 			}
- 			     i=i+1;
- 	}
- 	i=i+1;
-	while(i<len){
- 		if(str.charAt(i)=='"'){
- 			     	end=i;
- 			     	break;
-			}
- 			     i=i+1;
- 	}
-
-	if(start>end)
-		return null;
-
-	return str.substring(start,end);
-
-
-}
-public static String parseDict(String str,String k){
-	
-	int start=0;
-	int end=0;
-	int count=0;
-	int len=str.length();
- 	int i=str.indexOf(k);
- 	i=i+k.length()+1;
-
- 	while(i<len){
- 		if(str.charAt(i)=='{'){
- 			     	start=i+1;
- 			     	count=1;
- 			     	break;
- 		}
- 		i=i+1;
- 	}
- 	i=i+1;
-	while(i<len){
-
- 		if(str.charAt(i)=='{'){
- 			     	
- 			     	count=count+1;
- 			     
-		}else if(str.charAt(i)=='}'){
-
-						count=count-1;
-						if(count==0){
-							end=i;
-							break;
-						}
-
-				}
- 		i=i+1;
-					
- 	}
-
-	if(start>end)
-		return null;
-	return str.substring(start,end);
-	
-
-}
-public static String[] parseArray(String str,String k){
-
-	int start=0;
-	int end=0;
-	int count=0;
-	int len=str.length();
- 	int i=str.indexOf(k);
- 	i=i+k.length()+1;
-	while(i<len){
- 		if(str.charAt(i)=='['){
- 			     	start=i+1;
- 			     	count=1;
- 			     	break;
- 		}
- 		i=i+1;
- 	}
- 	i=i+1;
-	while(i<len){
-			if(str.charAt(i)=='['){
- 			     	count=count+1;
- 			     
-			}else if(str.charAt(i)==']'){
-						count=count-1;
-						if(count==0){
-							end=i;
-							break;
-						}
+			i=i+1;
+		}
+		i=i+1;
+		while(i<len){
+			if(str.charAt(i)=='"')
+			{
+				
+				end=i;
+				break;
 
 			}
- 			i=i+1;
+			i=i+1;
+		}
+		if(start>end)
+			return null;
+		return str.substring(start,end);
+
+
 	}
+	public static String parseDict(String str,String k){
+		int start=0;
+		int end=0;
+		int count=0;
+		int len=str.length();
+		int i=str.indexOf(k);
+		i=i+k.length()+1;
 
-	if(start>end)
-		return null;
-	return str.substring(start,end).split(",");
-}
+		while(i<len){
+			if(str.charAt(i)=='{')
+			{
+				start=i+1;
+				count=1;
+				break;
+			}
+			i=i+1;
+		}
+		i=i+1;
+		while(i<len){
+
+			if(str.charAt(i)=='{')
+			{
+				count=count+1;
+				
+			}else if(str.charAt(i)=='}'){
+
+				count=count-1;
+				if(count==0){
+					end=i;
+					break;
+				}
+
+			}
+			i=i+1;
+			
+		}
+		if(start>end)
+			return null;
+		return str.substring(start,end);
+		
+
+	}
+	public static ArrayList<String> parseArray(String str,String k){
+
+		int start=0;
+		int end=0;
+		int count=0;
+		int len=str.length();
+		int i=str.indexOf(k);
+		i=i+k.length()+1;
+		while(i<len){
+			if(str.charAt(i)=='[')
+			{
+				start=i+1;
+				count=1;
+				break;
+			}
+			i=i+1;
+		}
+		i=i+1;
+		while(i<len){
+			if(str.charAt(i)=='[')
+			{
+				count=count+1;
+				
+			}else if(str.charAt(i)==']'){
+
+				count=count-1;
+				if(count==0){
+					end=i;
+					break;
+				}
+
+			}
+			i=i+1;
+		}
+		if(start>end)
+			return null;
+		count=0;
+		int pre=0;
+		String tmp=str.substring(start,end);
+		i=0;
+
+		len=tmp.length();
+		ArrayList<String> res=new ArrayList<String>();
+		while(i<len)
+		{
+
+			if(tmp.charAt(i)=='['||tmp.charAt(i)=='{'){
+				count=count+1;
+			}else if(tmp.charAt(i)==']'||tmp.charAt(i)=='}'){
+				count=count-1;
+			}else if(tmp.charAt(i)==','&&count==0){
+				pre=i;
+				res.add(tmp.substring(0,i));
+			}
+			i=i+1;
+		}
+		res.add(tmp.substring(pre+1,tmp.length()));
+		return res;
+	}
 }
